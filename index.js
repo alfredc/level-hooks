d16040ce44f3e103160bfa4c95afb20dcfb7f11c
Alfred Chan <alfredwchan@gmail.com>
2013-08-29 15:41:26 +0900

    async prehooks

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ module.exports = function (db) {
   var posthooks = []
   var prehooks  = []
 
+  function asyncEach(arr, iterator, callback) {
+    var pending = arr.length
+    if(!pending) return callback(null)
+    arr.forEach(function (item, i) {
+      iterator.call(null, item, i, function (err) {
+        if(err) {
+          pending = 0
+          return callback(err)
+        }
+        if(!--pending) callback(null)
+      })
+    })
+  }
+
   function getPrefix (p) {
     return p && (
         'string' ===   typeof p        ? p
@@ -81,82 +95,86 @@ module.exports = function (db) {
   var batch = db.batch
 
   function callHooks (isBatch, b, opts, cb) {
-    try {
-    b.forEach(function hook(e, i) {
-      prehooks.forEach(function (h) {
-        if(h.test(String(e.key))) {
-          //optimize this?
-          //maybe faster to not create a new object each time?
-          //have one object and expose scope to it?
-          var context = {
-            add: function (ch, db) {
-              if(typeof ch === 'undefined') {
-                return this
-              }
-              if(ch === false)
-                return delete b[i]
-              var prefix = (
-                getPrefix(ch.prefix) || 
-                getPrefix(db) || 
-                h.prefix || ''
-              )
-              ch.key = prefix + ch.key
-              if(h.test(String(ch.key))) {
-                //this usually means a stack overflow.
-                throw new Error('prehook cannot insert into own range')
-              }
-              var ke = ch.keyEncoding   || getKeyEncoding(ch.prefix)
-              var ve = ch.valueEncoding || getValueEncoding(ch.prefix)
-              if(ke) ch.keyEncoding = ke
-              if(ve) ch.valueEncoding = ve
+    asyncEach(b, function hook(e, i, doneHooks) {
+      asyncEach(prehooks, function (h, j, doneHook) {
+        if(!h.test(String(e.key))) return doneHook(null)
 
-              b.push(ch)
-              hook(ch, b.length - 1)
-              return this
-            },
-            put: function (ch, db) {
-              if('object' === typeof ch) ch.type = 'put'
-              return this.add(ch, db)
-            },
-            del: function (ch, db) {
-              if('object' === typeof ch) ch.type = 'del'
-              return this.add(ch, db)
-            },
-            veto: function () {
-              return this.add(false)
+        //optimize this?
+        //maybe faster to not create a new object each time?
+        //have one object and expose scope to it?
+        var context = {
+          add: function (ch, db, done) {
+            if(typeof ch === 'undefined')
+              return done(null)
+            if(ch === false) {
+              delete b[i]
+              return done(null)
             }
-          }
-          h.hook.call(context, e, context.add, b)
-        }
-      })
-    })
-    } catch (err) {
-      return (cb || opts)(err)
-    }
-    b = b.filter(function (e) {
-      return e && e.type //filter out empty items
-    })
 
-    if(b.length == 1 && !isBatch) {
-      var change = b[0]
-      return change.type == 'put' 
-        ? put.call(db, change.key, change.value, opts, cb) 
-        : del.call(db, change.key, opts, cb)  
-    }
-    return batch.call(db, b, opts, cb)
+            var prefix = (
+              getPrefix(ch.prefix) || 
+              getPrefix(db) || 
+              h.prefix || ''
+            )
+            ch.key = prefix + ch.key
+            if(h.test(String(ch.key))) {
+              //this usually means a stack overflow.
+              return done(new Error('prehook cannot insert into own range'))
+            }
+            var ke = ch.keyEncoding   || getKeyEncoding(ch.prefix)
+            var ve = ch.valueEncoding || getValueEncoding(ch.prefix)
+            if(ke) ch.keyEncoding = ke
+            if(ve) ch.valueEncoding = ve
+
+            b.push(ch)
+            hook(ch, b.length - 1, done)
+          },
+          put: function (ch, db, done) {
+            if('object' === typeof ch) ch.type = 'put'
+            this.add(ch, db, done)
+          },
+          del: function (ch, db) {
+            if('object' === typeof ch) ch.type = 'del'
+            this.add(ch, db, done)
+          },
+          veto: function (done) {
+            this.add(false, null, done)
+          }
+        }
+
+        h.hook.call(context, e, context.add, b, doneHook)
+      }, doneHooks)
+    }, cb)
+  }
+
+  function execute(isBatch, b, opts, cb) {
+    callHooks(isBatch, b, opts, function (err) {
+      if(err) return cb(err)
+      b = b.filter(function (e) {
+        return e && e.type //filter out empty items
+      })
+
+      if(b.length == 1 && !isBatch) {
+        var change = b[0]
+        return change.type == 'put' 
+          ? put.call(db, change.key, change.value, opts, cb) 
+          : del.call(db, change.key, opts, cb)  
+      }
+      return batch.call(db, b, opts, cb)
+    })
   }
 
   db.put = function (key, value, opts, cb ) {
     var batch = [{key: key, value: value, type: 'put'}]
-    return callHooks(false, batch, opts, cb)
+    return execute(false, batch, opts, cb)
   }
 
   db.del = function (key, opts, cb) {
     var batch = [{key: key, type: 'del'}]
-    return callHooks(false, batch, opts, cb)
+    return execute(false, batch, opts, cb)
   }
 
   db.batch = function (batch, opts, cb) {
-    return callHooks(true, batch, opts, cb)
+    return execute(true, batch, opts, cb)
   }
 }
